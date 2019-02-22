606040e45a85d44cbe646cc7de259ab0861663b7
Brennon Miller <bjm121@pitt.edu>
2019-02-22 15:41:10 -0500

    Added initial Lists, AList class

---
+++ AList.java
@@ -0,0 +1,155 @@
+import java.util.Iterator;
+import java.util.Arrays;
+
+public class AList<T> implements List<T> {
+    private Object [] array;
+    private int size = 0;
+    private final static int DEFAULT_CAPACITY=25;
+    private final static int MAX_CAPACITY=10000;
+    
+    public AList(){
+        this(DEFAULT_CAPACITY);
+    }
+    
+    public AList(int capacity) {
+        array = new Object[capacity];
+        size = 0;
+    }
+    
+    @Override
+    public boolean add(T o) {
+        if (size < array.length || resizeArray(2 * size)) {
+            array[size] = o;
+            size++;
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean add(int index, T o) {
+        if (size < array.length || resizeArray(2 * size)) {
+            for (int i = size; i >= index; i--) {
+                array[i] = array[i - 1];
+            }
+            array[index] = o;
+            size++;
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public void clear() {
+        array = new Object[DEFAULT_CAPACITY];
+        size = 0;
+    }
+
+    @Override
+    public boolean contains(T o) {
+        return indexOf(o) != -1;
+    }
+
+    @Override
+    public T get(int index) {
+        if (index >= 0 && index < size)
+            return (T)array[index];
+        else
+            return null;
+    }
+
+    @Override
+    public T set(int index, T o) {
+        T retVal = (T)array[index];    // Save original value to return.
+        array[index] = o;
+        return retVal;
+    }
+
+    @Override
+    public boolean remove(T o) {
+        int index = indexOf(o);
+        if (index != -1) {
+            remove(index);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public T remove(int index) {
+        if (index >= 0 && index < size) {
+            T obj = (T)array[index];
+            for (int i = index; i < size-1; i++)
+                array[i] = array[i+1];
+            size--;
+            return obj;
+        } else
+            return null;
+    }
+
+    @Override
+    public int indexOf(T o) {
+        T obj;
+        for (int i = 0; i < size; i++) {
+            obj = (T)array[i];
+            if (obj.equals(o)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    @Override
+    public int lastIndexOf(T o) {
+        T obj;
+        for (int i = size - 1; i >= 0; i--) {
+            obj = (T)array[i];
+            if (obj.equals(o)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return size==0;
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new Iterator<T>() {
+            private int cursor = 0;
+            
+            @Override
+            public boolean hasNext() {
+                return cursor < size;
+            }
+
+            @Override
+            public T next() {
+                T obj = (T)array[cursor];
+                cursor++;
+                return obj;
+            }
+        };
+    }
+
+    @Override
+    public Object[] toArray() {
+        return Arrays.copyOf(array, size);
+    }
+    
+    private boolean resizeArray(int newCapacity) {
+        if (newCapacity > MAX_CAPACITY)
+                return false;
+        array = Arrays.copyOf(array, newCapacity);
+        return true;
+    }
+    
+}
--- OnuGame.java
+++ OnuGame.java
@@ -5,6 +5,16 @@
  */
 public class OnuGame {
     public static void main() {
-        // TODO: Something.
+        List<Card> deck = new AList();
+        List<Card> discard = new AList();
+
+        List<Card> player1 = new LList();
+        List<Card> player2 = new LList();
+        List<Card> player3 = new LList();
+
+        // TODO: Work out shuffling algorithm.
+        // Perhaps within `List`?
+
+        // TODO: Implement game loop.
     }
 }
