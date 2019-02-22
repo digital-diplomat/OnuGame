f56b3b8b8d60a09424e7a577a20b55d83e09636d
Brennon Miller <bjm121@pitt.edu>
2019-02-22 15:21:03 -0500

    Added main class.

---
+++ OnuGame.java
@@ -0,0 +1,10 @@
+/**
+ * OnuGame: An UNO clone for the command line.
+ * @author Vaughn Coates
+ * @author Brennon Miller
+ */
+public class OnuGame {
+    public static void main() {
+        // TODO: Something.
+    }
+}
